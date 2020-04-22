5b8e3f28b5ceba183335e0deeb398f4003b830eb
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-04-22 11:41:53 +0200

    ciclo for ottimizzato per fermarsi una volta trovata l'email

--- mail/script.js
+++ mail/script.js
@@ -4,10 +4,13 @@ var email_utente = prompt('Inserisci la tua email per accedere');
 //Crea una lista di emails abilitate all'accesso
 var email_list = ['abcd@gmail.com', 'qwer@gmail.com', 'asd@mail.com', '1234@gmail.com']
 
+//Dichiaro la var permesso per far ricordare allo script se la mail è abilitata o meno
+var permesso = false;
+
 //Confronta l'email utente con le emails abilitate e setta permesso a true se almeno una corrsiponde
-for (var i = 0; i < email_list.length; i++) {
+for (var i = 0; (i < email_list.length) && (permesso != true); i++) {
     if (email_list[i] == email_utente) {
-        var permesso = true;
+        permesso = true;
     }
 }
 
